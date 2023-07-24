baa638f0f1fde9abc991ab62361cb37047431bee
이선호 <tjsgh5312@daum.net>
2023-07-24 23:52:42 +0900

    check 함수를 잘못 적은것 같아요...

--- 1. 백트래킹/N-Queen/solution.py
+++ 1. 백트래킹/N-Queen/solution.py
@@ -1,9 +1,15 @@
 import sys
 input = sys.stdin.readline
 
+N = int(input())
+result = 0
+board = [[0]* N]*N
+
 def nqueen(row):
     if row == N-1:
+        print("여길 안오노")
         result += 1
+        print(board)
         return
     
     for i in range(N):
@@ -17,6 +23,7 @@ def check(row, col):
     for i in range(row-1):
         if board[i][col] == 1:
             return False
+        
     #2 대각선에 있는가
     for i in range(row-1):
         gap = row - i
@@ -24,14 +31,16 @@ def check(row, col):
         if col - gap > 0:
             if board[i][col-gap] == 1:
                 return False
+        
         # 오른쪽 대각선    
         if col + gap < N:
             if board[i][col+gap] == 1:
                 return False
     return True
 
-if __name__ == 'main':
-    N = int(input())
-    result = 0
-    board = [[0]* N]*N
-    nqueen(0)
+def printBoard():
+    for i in range(N):
+        print(board[i])
+
+nqueen(0)
+print(result)
